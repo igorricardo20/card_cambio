a711c8986e4c6007c8045b095cc9ddc60cb3ee2c
Igor Alves <igor.alves48@outlook.com>
2024-07-31 15:30:17 +0200

    Add url launcher to open data

--- lib/features/info/views/info.dart
+++ lib/features/info/views/info.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class Info extends StatelessWidget {
   const Info({super.key});
@@ -17,17 +19,38 @@ class Info extends StatelessWidget {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Info', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-                    Text('Open Data', style: TextStyle(fontSize: 20)),
+                    Text('Open Data', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 10),
+                    Text('Open data in Brazil helps promote transparency and innovation by making government data freely available to the public. The government and various institutions provide a wide range of datasets across different areas.')
                   ],
                 ),
                 SizedBox(height: 20),
                 Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start, 
                   children: [
-                    Text('More Text', style: TextStyle(fontSize: 20)),
-                    Text('Text 1'),
-                    Text('Text 2'),
+                    Text('Government Initiatives', style: TextStyle(fontSize: 20)),
+                    SizedBox(height: 10),
+                    Text('The platform where the Brazilian government publishes open data is called Portal Brasileiro de Dados Abertos (Brazilian Open Data Portal). It includes datasets from various ministries and agencies, covering topics like health, education, economy, environment, and more.'),
+                    SizedBox(height: 20),
+                    //textSpan widget with onTap to the link
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Visit the ',
+                          ),
+                          TextSpan(
+                            text: 'Brazilian Open Data Portal',
+                            style: TextStyle(color: Colors.blue, fontSize: 14),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () => launchUrlString('https://dados.gov.br/'),
+                          ),
+                          TextSpan(
+                            text: ' to learn more.',
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ]
