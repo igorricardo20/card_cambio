08b43a664055759727272ac71cd47728f7c77cb1
Igor Alves <igor.alves48@outlook.com>
2024-11-21 16:33:25 +0100

    Update About view with enhanced content and layout adjustments for improved presentation

--- lib/features/info/views/about.dart
+++ lib/features/info/views/about.dart
@@ -6,10 +6,10 @@ class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
       child: Center(
         child: ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: 1000),
+          constraints: BoxConstraints(maxWidth: 600),
           child: Padding(
             padding: const EdgeInsets.only(left: 1.0),
             child: ListView(
@@ -17,20 +17,76 @@ class About extends StatelessWidget {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('About Us', style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
+                    Text(
+                      'About This App',
+                      style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'This app is a showcase of what we do best: building fast, reliable, and visually stunning software. '
+                      'Designed to highlight our capabilities, it serves as an example of the quality and creativity we bring to every project. '
+                      'From seamless functionality to polished design, this app demonstrates how we turn ideas into impactful digital experiences.',
+                      style: TextStyle(fontSize: 16, height: 1.5),
+                    ),
                   ],
                 ),
-                SizedBox(height: 20),
+                SizedBox(height: 30),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Rockpeach', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900)),
+                    Text(
+                      'About Us',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text.rich(
+                      TextSpan(
+                        text: 'At ',
+                        style: TextStyle(fontSize: 16, height: 1.5),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Rockpeach',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: ', we believe technology is more than just code – it’s about crafting experiences that connect people and businesses. '
+                                'We are a software house dedicated to developing agile, reliable, and visually compelling solutions tailored to our clients’ unique needs.\n\n'
+                                'We quickly turn concepts into products that drive success. Our mission is to simplify complexity while delivering applications with impeccable design.\n\n'
+                                'Trust us to transform challenges into opportunities and ideas into reality.',
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    // Text(
+                    //   'Rockpeach',
+                    //   style: TextStyle(fontSize: 28, fontWeight: FontWeight.w900, fontStyle: FontStyle.italic),
+                    // ),
+                    // the same as above but within a linear gradient of blue and red
+                    Text(
+                      'Rockpeach',
+                      style: TextStyle(
+                        fontSize: 28, 
+                        fontWeight: FontWeight.w900, 
+                        fontStyle: FontStyle.italic, 
+                        foreground: Paint()..shader = LinearGradient(colors: [Colors.blue, Colors.redAccent]).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0))),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'https://www.rockpeach.io/',
+                      style: TextStyle(fontSize: 16, color: Colors.blue),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      'Utrecht, Netherlands',
+                      style: TextStyle(fontSize: 16),
+                    ),
                   ],
                 ),
-              ]
+              ],
             ),
           ),
-        )
+        ),
       ),
     );
   }
